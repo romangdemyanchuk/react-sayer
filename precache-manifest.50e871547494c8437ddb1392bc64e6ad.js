b4a43ec2d428611329a357ba0383d67128447cd9
Roman <roman.g.demyanchuk@gmail.com>
2020-04-13 19:23:58 +0300

    Updates

---
+++ precache-manifest.50e871547494c8437ddb1392bc64e6ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072032a9bd46140799184d0bec050a90",
+    "url": "/react-sayer/index.html"
+  },
+  {
+    "revision": "b14f43e6794f7d07590b",
+    "url": "/react-sayer/static/css/main.7f0c6bc9.chunk.css"
+  },
+  {
+    "revision": "3af39620ecde8ae56b2d",
+    "url": "/react-sayer/static/js/2.acebfde0.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/react-sayer/static/js/2.acebfde0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14f43e6794f7d07590b",
+    "url": "/react-sayer/static/js/main.23378c91.chunk.js"
+  },
+  {
+    "revision": "42c46ab580fe4f230cd4",
+    "url": "/react-sayer/static/js/runtime-main.ad379502.js"
+  }
+]);
